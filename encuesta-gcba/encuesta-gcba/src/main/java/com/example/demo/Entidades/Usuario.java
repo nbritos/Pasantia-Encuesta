82f05caa9c82e86eb4a264bf26d271e074ccab8e
NicoBritos <nbritos1988@gmail.com>
2023-10-30 23:55:26 -0300

    "get y put usuario hecho"

--- encuesta-gcba/encuesta-gcba/src/main/java/com/example/demo/Entidades/Usuario.java
+++ encuesta-gcba/encuesta-gcba/src/main/java/com/example/demo/Entidades/Usuario.java
@@ -2,8 +2,7 @@ package com.example.demo.Entidades;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+
 @Entity
 public class Usuario {
     @Id
